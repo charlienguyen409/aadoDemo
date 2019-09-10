8e11e2bd5b393b867cc6049dbe05195317a9f01d
charlienguyen409 <charlienguyen409@gmail.com>
2019-09-10 08:49:08 -0400

    Update Startup.cs

--- AADO/Startup.cs
+++ AADO/Startup.cs
@@ -27,7 +27,7 @@ namespace AADO
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello World!!!");
             });
         }
     }
